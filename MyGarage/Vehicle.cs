17001f1b911ad93d8ecc5507a1042b47df5c91a9
denise baker <denviol@yahoo.com>
2020-03-16 05:23:23 -0500

    setup intitial vehicles files

---
+++ MyGarage/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyGarage
+{
+    class Vehicle
+    {
+        public int FuelCapacity { get; set; }
+        public int Occupancy { get; set; }
+        public string Color { get; set; }
+
+
+        public Vehicle(int fuelCapacity, int occupancy, string color)
+        {
+            FuelCapacity = fuelCapacity;
+            Occupancy = occupancy;
+            Color = color;
+        }
+    }
+}
